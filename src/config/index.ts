14adf426892a8b9d5277bfb8ce8b100c03012c1a
Corbin Page <corbin.page@gmail.com>
2020-12-31 14:39:58 -0500

    Added some extra loggin

--- src/config/index.ts
+++ src/config/index.ts
@@ -11,6 +11,11 @@ const configs = {
 const env = process.env.VUE_APP_ENV || 'production';
 const network = process.env.VUE_APP_NETWORK || 'homestead';
 
+console.log('Env')
+console.log(process.env.VUE_APP_PROTEKT_REDEEM_TOKEN_SYMBOL)
+console.log(process.env.VUE_APP_ENV)
+console.log(process.env.VUE_APP_NETWORK)
+
 let config
 if(process.env.VUE_APP_PROTEKT_REDEEM_TOKEN_SYMBOL) {
   config = configs['token'][process.env.VUE_APP_PROTEKT_REDEEM_TOKEN_SYMBOL];
