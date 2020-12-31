8db1201c33b21a524a5dd2f486fb9db726ed1058
Corbin Page <corbin.page@gmail.com>
2020-12-31 14:28:30 -0500

    Set up TPTK and TCOMP on kovan

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,14 +2,20 @@ import homestead from '@/config/homestead.json';
 import kovan from '@/config/kovan.json';
 import rinkeby from '@/config/rinkeby.json';
 import TCOMP from '@/config/TCOMP.json';
+import TPTK from '@/config/TPTK.json';
 
 const configs = {
-  production: { homestead, kovan, rinkeby }
+  production: { homestead, kovan, rinkeby },
+  token: { TPTK, TCOMP }
 };
 const env = process.env.VUE_APP_ENV || 'production';
 const network = process.env.VUE_APP_NETWORK || 'homestead';
 
-const config = TCOMP;
-// config = configs[env][network];
+let config
+if(process.env.VUE_APP_PROTEKT_REDEEM_TOKEN_SYMBOL) {
+  config = configs['token'][process.env.VUE_APP_PROTEKT_REDEEM_TOKEN_SYMBOL];
+} else {
+  config = configs[env][network];
+}
 
 export default config;
